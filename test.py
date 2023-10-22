16863457e05f5b5d86291813c8bbaa8f45d66752
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-10-22 14:22:21 +0530

    Popularity-based rec::Weighted-rating for movies

--- test.py
+++ test.py
@@ -4,6 +4,22 @@ movies = pd.read_csv('movies.csv')
 credits = pd.read_csv('credits.csv')
 ratings = pd.read_csv('ratings.csv')
 
+# Calculate Weighted Rating
+# WR = (v / (v + m)) * R + (m / (v + m)) * C
+# v - number of votes for a movie
+# m - minimum number of votes required
+# R - average rating of the movie
+# C - average rating across all movies
+
+m = movies['vote_count'].quantile(0.9)
+C = movies['vote_average'].mean()
+
+def weighted_average(df, m=m, C=C):
+    R = df['vote_average']             
+    v = df['vote_count']
+    WR = (v / (v + m)) * R + (m / (v + m)) * C
+    return WR
+
+movies['weigthed_ratings'] = movies.apply(weighted_average, axis=1)
 print(movies.head())
-print(credits.head())
-print(ratings.head())
+
