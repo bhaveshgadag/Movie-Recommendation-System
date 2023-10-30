d841a56860847de8aa02dac7f5f0498432715e51
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-10-30 17:13:47 +0530

    similarity matrix::linear_kernel

--- content_based_filtering.py
+++ content_based_filtering.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 movies = pd.read_csv("movies_small.csv", sep=';')
-tfidf = TfidfVectorizer(stop_words='english')
+# print(movies)
 
+# Term Frequency Inverse Document frequeny
+tfidf = TfidfVectorizer(stop_words='english')
 tfidf_matrix = tfidf.fit_transform(movies['overview'])
 
-print(pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf.get_feature_names_out()))
+# print(pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf.get_feature_names_out()))
+
+# Similarity Matrix
+similarity_matrix = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+print(similarity_matrix)
