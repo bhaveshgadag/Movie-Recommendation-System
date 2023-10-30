359f8741e9abc95559ae0a418275b666ebc62b92
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-10-30 17:59:14 +0530

    fn to get similar movies using title

--- content_based_filtering.py
+++ content_based_filtering.py
@@ -14,4 +14,14 @@ tfidf_matrix = tfidf.fit_transform(movies['overview'])
 # Similarity Matrix
 similarity_matrix = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-print(similarity_matrix)
+# print(similarity_matrix)
+
+def similar_movies(movie_title, no_of_movies):
+    index = movies.loc[movies['title']==movie_title].index[0]
+    similarity_scores = list(enumerate(similarity_matrix[index]))
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    similar_movies_index = [tpl[0] for tpl in similarity_scores[1:no_of_movies + 1]]
+    similar_movies = list(movies['title'].iloc[similar_movies_index])
+    return similar_movies
+
+print(similar_movies('Cars 2', 2))
